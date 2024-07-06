85b7b5fecc6c8b291d9b40412668a0c049e8ecef
lenthanhnam <lengocthanhnam@gmail.com>
2024-07-06 20:25:01 +0700

    Add files via upload

---
+++ VanChuyenNgoai.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace QLCP6_2.Module.BusinessObjects.QuanLyQuanCaPhe6
+{
+    [DefaultClassOptions]
+    public partial class VanChuyenNgoai
+    {
+        public VanChuyenNgoai(Session session) : base(session) { }
+        public override void AfterConstruction() {
+            if (Session.IsNewObject(this))
+            {
+                NgayGiaoHang = DateTime.Now;
+            }
+            base.AfterConstruction(); }
+    }
+
+}
---
+++ VanChuyenNoiBo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using DevExpress.Persistent.Base;
+namespace QLCP6_2.Module.BusinessObjects.QuanLyQuanCaPhe6
+{
+    [DefaultClassOptions]
+    public partial class VanChuyenNoiBo
+    {
+        public VanChuyenNoiBo(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+
+}
